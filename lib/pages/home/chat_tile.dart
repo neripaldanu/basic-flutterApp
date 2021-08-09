cf554dcac5a3ea43a6798908d081519b6f6eda0f
neripaldanu <paldanuneri@gmail.com>
2021-08-09 21:59:39 +0800

    tambah list chat content

---
+++ lib/pages/home/chat_tile.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:pringgosadventure/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 33),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/image_logo_shop.png',
+                width: 54,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shoe Store',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Good night, This item is on...',
+                      style: secondaryTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Now',
+                style: secondaryTextStyle.copyWith(
+                  fontSize: 10,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Divider(
+            thickness: 1,
+            color: Color(0xff2B2939),
+          ),
+        ],
+      ),
+    );
+  }
+}
